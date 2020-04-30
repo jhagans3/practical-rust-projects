d76e6f0c7bd623aea370231f019f8062a47f54e8
James Hagans <jhagans@acm.org>
2020-04-30 01:16:21 -0400

    run rustfmt

--- catsay/src/main.rs
+++ catsay/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let message = std::env::args()
-                    .nth(1) // the 0 arg is the name of the binary
-                    .expect("Missing the message. Usage: catsay < message>");
+        .nth(1) // the 0 arg is the name of the binary
+        .expect("Missing the message. Usage: catsay < message>");
     println!("{}", message);
     println!(" \\");
     println!("  \\");
