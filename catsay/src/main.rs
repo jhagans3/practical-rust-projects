14f843a41833abb77350b8c5ceafd85bbd71d870
James Hagans <jhagans@acm.org>
2020-04-30 01:58:48 -0400

    add StructOpt

--- catsay/src/main.rs
+++ catsay/src/main.rs
@@ -1,11 +1,38 @@
+// extern crate structopt;
+
+use structopt::StructOpt;
+// cargo run -- --help
+#[derive(StructOpt, Debug)]
+struct Options {
+
+    /// What does the cat say?
+    #[structopt(short, long, default_value = "Meow!")]
+    message: String,
+
+    /// Make the cat appear dead
+    #[structopt(short = "d", long = "dead")]
+    dead: bool,
+}
+
+
 fn main() {
-    let message = std::env::args()
-        .nth(1) // the 0 arg is the name of the binary
-        .expect("Missing the message. Usage: catsay < message>");
+    let options = Options::from_args();
+    let message = options.message;
+    let status = if options.dead {
+        "x"
+    } else {
+        "o"
+    };
+
+    // cargo run -- -m "woof" 1> stdout.txt 2> stderr.txt
+    if message.to_lowercase() == "woof" {
+        eprintln!("A cat shouldn't bark like a dog.")
+    }
+
     println!("{}", message);
-    println!(" \\");
-    println!("  \\");
-    println!("     /\\_/\\");
-    println!("    ( o o )");
-    println!("    =( I )=");
+    println!("{pad}\\", pad=" ".repeat(1));
+    println!("{pad}\\", pad=" ".repeat(2));
+    println!("{pad}/\\_/\\", pad=" ".repeat(5));
+    println!("{pad}( {eye} {eye} )", pad=" ".repeat(4), eye=status);
+    println!("{pad}=( I )=", pad=" ".repeat(4));
 }
