bd34a75b15bff7d6d6b72824b1f836e0c1035fd8
James Hagans <jhagans@acm.org>
2020-04-30 02:10:48 -0400

    add colored text

--- catsay/src/main.rs
+++ catsay/src/main.rs
@@ -1,10 +1,9 @@
-// extern crate structopt;
-
 use structopt::StructOpt;
+use colored::*;
+
 // cargo run -- --help
 #[derive(StructOpt, Debug)]
 struct Options {
-
     /// What does the cat say?
     #[structopt(short, long, default_value = "Meow!")]
     message: String,
@@ -14,25 +13,20 @@ struct Options {
     dead: bool,
 }
 
-
 fn main() {
     let options = Options::from_args();
     let message = options.message;
-    let status = if options.dead {
-        "x"
-    } else {
-        "o"
-    };
+    let status = if options.dead { "x".red().bold() } else { "o".green().bold() };
 
     // cargo run -- -m "woof" 1> stdout.txt 2> stderr.txt
     if message.to_lowercase() == "woof" {
         eprintln!("A cat shouldn't bark like a dog.")
     }
 
-    println!("{}", message);
-    println!("{pad}\\", pad=" ".repeat(1));
-    println!("{pad}\\", pad=" ".repeat(2));
-    println!("{pad}/\\_/\\", pad=" ".repeat(5));
-    println!("{pad}( {eye} {eye} )", pad=" ".repeat(4), eye=status);
-    println!("{pad}=( I )=", pad=" ".repeat(4));
+    println!("{}", message.bright_yellow().underline().on_purple());
+    println!("{pad}\\", pad = " ".repeat(1));
+    println!("{pad}\\", pad = " ".repeat(2));
+    println!("{pad}/\\_/\\", pad = " ".repeat(5));
+    println!("{pad}( {eye} {eye} )", pad = " ".repeat(4), eye = status);
+    println!("{pad}=( I )=", pad = " ".repeat(4));
 }
