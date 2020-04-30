e321ae185ae17932a3da2640ad7822e00f1dbdeb
James Hagans <jhagans@acm.org>
2020-04-30 01:08:45 -0400

    add std args

--- catsay/src/main.rs
+++ catsay/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let message = std::env::args()
+                    .nth(1) // the 0 arg is the name of the binary
+                    .expect("Missing the message. Usage: catsay < message>");
+    println!("{}", message);
+    println!(" \\");
+    println!("  \\");
+    println!("     /\\_/\\");
+    println!("    ( o o )");
+    println!("    =( I )=");
 }
