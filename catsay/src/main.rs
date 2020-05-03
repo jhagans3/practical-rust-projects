e4cfc15bba8840876d1004fa84781a9fbad36444
James Hagans <jhagans@acm.org>
2020-05-03 00:10:43 -0400

    add file reading

--- catsay/src/main.rs
+++ catsay/src/main.rs
@@ -1,5 +1,5 @@
-use structopt::StructOpt;
 use colored::*;
+use structopt::StructOpt;
 
 // cargo run -- --help
 #[derive(StructOpt, Debug)]
@@ -9,14 +9,24 @@ struct Options {
     message: String,
 
     /// Make the cat appear dead
-    #[structopt(short = "d", long = "dead")]
+    #[structopt(short, long)]
     dead: bool,
+
+    /// Load the cat picture from the specified file
+    #[structopt(short = "f", long = "file", parse(from_os_str))]
+    catfile: Option<std::path::PathBuf>,
 }
 
 fn main() {
     let options = Options::from_args();
     let message = options.message;
-    let status = if options.dead { "x".red().bold() } else { "o".green().bold() };
+
+    // NO_COLOR=1 cargo run
+    let status = if options.dead {
+        "x".red().bold()
+    } else {
+        "o".green().bold()
+    };
 
     // cargo run -- -m "woof" 1> stdout.txt 2> stderr.txt
     if message.to_lowercase() == "woof" {
@@ -24,9 +34,19 @@ fn main() {
     }
 
     println!("{}", message.bright_yellow().underline().on_purple());
-    println!("{pad}\\", pad = " ".repeat(1));
-    println!("{pad}\\", pad = " ".repeat(2));
-    println!("{pad}/\\_/\\", pad = " ".repeat(5));
-    println!("{pad}( {eye} {eye} )", pad = " ".repeat(4), eye = status);
-    println!("{pad}=( I )=", pad = " ".repeat(4));
+    match &options.catfile {
+        Some(path) => {
+            let cat_template =
+                std::fs::read_to_string(path).expect(&format!("could not read file {:?}", path));
+            let cat_picture = cat_template.replace("{eye}", "o");
+            println!("{}", &cat_picture);
+        }
+        None => {
+            println!("{pad}\\", pad = " ".repeat(1));
+            println!("{pad}\\", pad = " ".repeat(2));
+            println!("{pad}/\\_/\\", pad = " ".repeat(5));
+            println!("{pad}( {eye} {eye} )", pad = " ".repeat(4), eye = status);
+            println!("{pad}=( I )=", pad = " ".repeat(4));
+        }
+    }
 }
