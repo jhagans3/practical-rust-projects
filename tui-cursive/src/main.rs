52a8016ae03c5ab2bcd95e2a74716aa1ccf7d7d4
James Hagans <jhagans@acm.org>
2020-05-05 01:26:49 -0400

    add multi-layer step flow

--- tui-cursive/src/main.rs
+++ tui-cursive/src/main.rs
@@ -1,8 +1,79 @@
 use cursive::Cursive;
 use cursive::event::Key;
-use cursive::views::{Dialog, TextView};
+// for .with_id() and .call_on_id() 
+use cursive::traits::Identifiable;
+use cursive::views::{Checkbox, Dialog, EditView, ListView, TextView};
+
+
+// wrap all form fields value in one struct so we can pass
+// around easily
+struct CatsayOptions<'a> {
+    message: &'a str,
+    dead: bool,
+}
+
+fn input_step(siv: &mut Cursive) {
+    siv.add_layer(
+Dialog::new()
+        .title("Please fill out the form for the cat") 
+// setting the title            
+        .content(
+            ListView::new()
+            .child(
+                "Message:",
+                EditView::new().with_name("message")
+            )
+            .child(
+                "Dead?", 
+                Checkbox::new().with_name("dead")
+            ),
+        )
+        .button("OK", |s| {
+            let message = s
+                .call_on_name(
+                    "message",|t: &mut EditView| t.get_content()
+                ).unwrap();
+            let is_dead = s.call_on_name(
+                "dead",|t: &mut Checkbox| t.is_checked()
+            ).unwrap();
+            let options = CatsayOptions {
+                message: &message,
+                dead: is_dead,
+            };
+            result_step(s, &options)
+        }),
+    );
+}
+
+fn result_step(siv: &mut Cursive, options: &CatsayOptions) {
+    let eye = if options.dead { "x" } else { "o" };
+    let cat_text = format!(
+        "{msg}
+\\
+ \\    
+ /\\_/\\   
+( {eye} {eye} )   
+=( I )=",
+        msg = options.message,
+        eye = eye
+    );
+    siv.pop_layer();
+    siv.add_layer(
+        Dialog::around(TextView::new(cat_text))
+        .title("The cat says...")
+        .button("OK", |s| s.quit()),
+    );
+
+}
+
+
 
 fn main() {
+    let mut siv = Cursive::default();
+    input_step(&mut siv); 
+    siv.run();
+
+/*
     let mut siv = Cursive::default();
 
     let cat_text = "Meow!
@@ -25,5 +96,7 @@ fn main() {
     siv.add_global_callback(Key::Esc, |s| s.quit());
 
     // start event loop
+    // cargo run --bin tui-cursive
     siv.run();
+*/
 }
