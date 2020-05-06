36bcc38ed506e9c2f84fbdf5c1acc52f44311e59
James Hagans <jhagans@acm.org>
2020-05-06 00:36:12 -0400

    remove dead code

--- tui-cursive/src/main.rs
+++ tui-cursive/src/main.rs
@@ -71,32 +71,10 @@ fn result_step(siv: &mut Cursive, options: &CatsayOptions) {
 fn main() {
     let mut siv = Cursive::default();
     input_step(&mut siv); 
-    siv.run();
-
-/*
-    let mut siv = Cursive::default();
-
-    let cat_text = "Meow!
- \\
-  \\
-    /\\_/\\
-   ( o o )
-   =( I )=";
-
-
-   siv.add_layer(
-        Dialog::around(TextView::new(cat_text))
-            .button("OK", |s| s.quit())
-    );
-
-    // Declaring the app layout
-    // siv.add_layer(TextView::new(cat_text));
 
     // Listen to Key::Esc and quit
     siv.add_global_callback(Key::Esc, |s| s.quit());
 
-    // start event loop
     // cargo run --bin tui-cursive
     siv.run();
-*/
 }
